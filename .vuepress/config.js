cf70e8e91333b12c22773734a1cb139e77f1ea38
learnleft <112842791+learnleft@users.noreply.github.com>
2022-09-05 06:42:31 +0000

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,23 +1,16 @@
-
 module.exports = {
-    themeConfig: {// 主题设置
-        nav: [// 导航栏
-            {
-                text: '概述',
-                link: '/'
-            }, {
-                text: 'Vue 学习笔记',
-                items:[
-                    {text:'笔记', link: '/book/0-1'}, // 可不写后缀 .md
-                    {text:'其它链接', link: 'https://www.baidu.com/'}// 外部链接
-                ]
-            }, {
-                text: 'Typescript 学习笔记',
-                items:[
-                    {text:'笔记', link: '/'},// 以 ‘/’结束，默认读取 README.md
-                    {text:'其它链接', link: 'https://www.baidu.com/'} // 外部链接
-                ]
-            }
-        ]
+        sidebar: {//左侧列表
+            '/guide/vue/': [
+                'test01', 'test02', 'test03'
+            ],
+            '/guide/ts/': [
+                {
+                    title: 'Typescript 学习',
+                    collapsable: true,
+                    children: ['test01', 'test02', 'test03']
+                }
+            ],            
+            '/': [''] //不能放在数组第一个，否则会导致右侧栏无法使用 
+        }
 ...
 }
